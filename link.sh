7496ec49657bc1312b54d84b5fe15ea8a344ecb8
Trevor Bekolay <tbekolay@gmail.com>
2014-03-25 11:38:30 -0400

    Added homebrew lists

--- link.sh
+++ link.sh
@@ -1,20 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 declare -A TARGETS
 TARGETS[nanorc.d]=.nanorc.d
 TARGETS[xkb]=.xkb
 TARGETS[gitconfig]=.gitconfig
 TARGETS[install.apt]=.install.apt
+TARGETS[install.brew]=.install.brew
 TARGETS[nanorc]=.nanorc
 TARGETS[redshift.conf]=.config/redshift.conf
 TARGETS[remove.apt]=.remove.apt
+TARGETS[remove.brew]=.remove.brew
 TARGETS[rtorrent.rc]=.rtorrent.rc
 
-for DOTFILE in "${!TARGETS[@]}"; do
-    SRC="$HOME/Code/dotfiles/$DOTFILE"
-    DST="$HOME/${TARGETS[$DOTFILE]}"
+checkandlink () {
+    SRC=$1
+    DST=$2
     if [[ ! -h $DST || `readlink $DST` != $SRC ]]; then
         echo "--- Linking $DST to $SRC"
         rm -rf "$DST"
         ln -s "$SRC" "$DST"
     fi
+}
+
+for DOTFILE in "${!TARGETS[@]}"; do
+    SRC="$HOME/Code/dotfiles/$DOTFILE"
+    DST="$HOME/${TARGETS[$DOTFILE]}"
+    checkandlink "$SRC" "$DST"
 done
