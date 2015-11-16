dd0d938ec494dd7fd8bc0c70724219222e97c4c7
Trevor Bekolay <tbekolay@gmail.com>
2015-11-16 04:07:24 -0500

    Minor tweaks for a few files

--- jupyter.js
+++ jupyter.js
@@ -5,7 +5,7 @@ require([
 
     function to(mode) {
         function to_mode(c) {
-            return c.code_mirror.setOption('keyMap', mode);
+            c.code_mirror.setOption('keyMap', mode);
         };
         IPython.notebook.get_cells().map(to_mode);
         require("notebook/js/cell").Cell.options_default.cm_config.keyMap = mode;
@@ -13,8 +13,12 @@ require([
 
     events.on('notebook_loaded.Notebook', function () {
         require(["codemirror/keymap/emacs"], function () {
-            to('emacs')
-            console.log('emacs.js loaded')
+            to('emacs');
+            console.log('emacs.js loaded');
         });
+
+        // Also remove Ctrl-Shift-- as Emacs uses this for undo
+        IPython.keyboard_manager.edit_shortcuts.remove_shortcut('ctrl-shift--');
+        IPython.keyboard_manager.edit_shortcuts.remove_shortcut('ctrl-shift-subtract');
     });
 });
