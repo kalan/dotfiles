07eee582ff0f2f0f2c62a9bdaad267fb1a1e95d9
Trevor Bekolay <tbekolay@gmail.com>
2016-01-06 16:29:48 -0500

    Move toolbar button to jupyter config

--- jupyter.js
+++ jupyter.js
@@ -22,3 +22,28 @@ require([
         IPython.keyboard_manager.edit_shortcuts.remove_shortcut('ctrl-shift-subtract');
     });
 });
+
+require([
+    'base/js/namespace',
+    'base/js/events',
+], function (IPython, events) {
+    events.on('notebook_loaded.Notebook', function () {
+        if($(IPython.toolbar.selector.concat(' > #kill-run-first')).length == 0){
+            IPython.toolbar.add_buttons_group([
+                {
+                    'label'   : 'kill and run-first',
+                    'icon'    : 'fa fa-angle-double-down',
+                    'callback': function(){
+                        IPython.notebook.kernel.restart();
+                        $(IPython.events).one('kernel_ready.Kernel', function(){
+                            var idx = IPython.notebook.get_selected_index();
+                            IPython.notebook.select(0);
+                            IPython.notebook.execute_cell();
+                            IPython.notebook.select(idx);
+                        });
+                    }
+                }
+            ], 'kill-run-first');
+        }
+    });
+});
